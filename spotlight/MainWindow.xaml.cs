f450bd224c15ec33f0de9cf36de9d4a32b7d5169
yadro <yadrone@gmail.com>
2016-11-13 22:50:27 +0300

    Filtred by input text

--- spotlight/MainWindow.xaml.cs
+++ spotlight/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interop;
@@ -11,23 +13,22 @@ using System.Windows.Media.Imaging;
 
 namespace spotlight
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<FileInformation> FileInformations;
+
         public MainWindow()
         {
             InitializeComponent();
-            Icon extractAssociatedIcon = System.Drawing.Icon.ExtractAssociatedIcon(@"C:\WINDOWS\system32\notepad.exe");
 
-
-            string[] files = Directory.GetFiles("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs");
+            var searchEngine = new SearchEngine();
+            var files = searchEngine.GetFileListDeep("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs");
             List<FileInformation> items = new List<FileInformation>();
             foreach (string file in files)
             {
                 items.Add(new FileInformation(file));
             }
+            FileInformations = items;
             listBox.ItemsSource = items;
         }
 
@@ -35,6 +36,12 @@ namespace spotlight
         {
             FileInformation fileInformation = (FileInformation) ((Grid) sender).DataContext;
             Process.Start(fileInformation.FileLocation);
+        }
+
+        private void OnSearchInput(object sender, TextChangedEventArgs e)
+        {
+            string text = ((TextBox) sender).Text;
+            listBox.ItemsSource = FileInformations.Where(file => (file.DisplayName.ToLower().StartsWith(text.ToLower())));
         }
     }
 }
---
+++ spotlight/SearchEngine.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace spotlight
+{
+    public class SearchEngine
+    {
+        private const string AppsPath = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs";
+        private string[] cachedFileList;
+
+        public SearchEngine()
+        {
+        }
+
+        List<string> GetFileList(string[] folders)
+        {
+            List<string> result = new List<string>();
+            foreach (var folder in folders)
+            {
+               // Directory.GetDirectories()
+            }
+            return result;
+        }
+
+        public List<string> GetFileListDeep(string path)
+        {
+            return GetFileListDeep(path, "*", new List<string>());
+        }
+
+        public List<string> GetFileListDeep(string path, string filter)
+        {
+            return GetFileListDeep(path, filter, new List<string>());
+        }
+
+        private List<string> GetFileListDeep(string path, string filter, List<string> cache)
+        {
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                cache = GetFileListDeep(dir, filter, cache);
+            }
+            string[] files = Directory.GetFiles(path, filter);
+            cache.AddRange(files);
+            return cache;
+        }
+    }
+}
