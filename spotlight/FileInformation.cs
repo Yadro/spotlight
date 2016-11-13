3571df9f112e386ffbd6e489209752e5d9f886b5
yadro <yadrone@gmail.com>
2016-11-13 20:59:22 +0300

    Add FileInformation. Run programs on click.

---
+++ spotlight/FileInformation.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace spotlight
+{
+    public class FileInformation
+    {
+        public FileInformation(string fileLocation)
+        {
+            FileLocation = fileLocation;
+        }
+
+        public string FileLocation { get; }
+
+        public string DisplayName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(displayName))
+                    displayName = Path.GetFileNameWithoutExtension(FileLocation);
+                return displayName;
+            }
+        }
+
+        public string Extension
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(extension))
+                    extension = Path.GetExtension(FileLocation);
+                return extension;
+            }
+        }
+
+        public ImageSource Icon
+        {
+            get { return icon ?? (icon = GetAssociatedIcon(FileLocation)); }
+        }
+
+        private string displayName;
+        private string extension;
+        private ImageSource icon;
+        private string RealFileLocation;
+        
+
+        private static ImageSource GetAssociatedIcon(string fileName)
+        {
+            return ToImageSource(
+                System.Drawing.Icon.ExtractAssociatedIcon(fileName)
+            );
+        }
+
+        private static ImageSource ToImageSource(Icon icon)
+        {
+            ImageSource imageSource = Imaging.CreateBitmapSourceFromHIcon(
+                icon.Handle,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            return imageSource;
+        }
+    }
+}
--- spotlight/MainWindow.xaml.cs
+++ spotlight/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using System.Drawing;
 using System.IO;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,39 +23,18 @@ namespace spotlight
 
 
             string[] files = Directory.GetFiles("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs");
-            List<ListItems> items = new List<ListItems>();
+            List<FileInformation> items = new List<FileInformation>();
             foreach (string file in files)
             {
-                items.Add(new ListItems()
-                {
-                    name = file,
-                    icon = GetAssociatedIcon(file)
-                });
+                items.Add(new FileInformation(file));
             }
             listBox.ItemsSource = items;
         }
 
-        public static ImageSource GetAssociatedIcon(string fileName)
+        private void UIElement_OnMouseUp(object sender, MouseButtonEventArgs e)
         {
-            return ToImageSource(
-                System.Drawing.Icon.ExtractAssociatedIcon(fileName)
-            );
-        }
-
-        public static ImageSource ToImageSource(Icon icon)
-        {
-            ImageSource imageSource = Imaging.CreateBitmapSourceFromHIcon(
-                icon.Handle,
-                Int32Rect.Empty,
-                BitmapSizeOptions.FromEmptyOptions());
-
-            return imageSource;
-        }
-
-        class ListItems
-        {
-            public string name { get; set; }
-            public ImageSource icon { get; set; }
+            FileInformation fileInformation = (FileInformation) ((Grid) sender).DataContext;
+            Process.Start(fileInformation.FileLocation);
         }
     }
 }
