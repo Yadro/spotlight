01e1b81c28e0e73335a175621612521df33b9659
yadro <yadrone@gmail.com>
2016-11-15 09:26:08 +0300

    Replace file

--- spotlight/FileInformation.cs
+++ spotlight/FileInformation.cs
@@ -4,8 +4,9 @@ using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using spotlight.ListItem;
 
-namespace spotlight.ListItem
+namespace spotlight
 {
     public class FileInformation : SearchItem
     {
